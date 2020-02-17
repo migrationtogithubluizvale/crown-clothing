20df4ef0e747e9a78dc26145ddca7a92adf38d9a
Luiz Felipe Silva Vale <luiz.vale1995@gmail.com>
2020-02-17 15:29:40 -0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component'
 
+const TopicDetail = (props) => {
+  console.log(props)
+  return <div>
+    <h1>Topic Details</h1>
+  </div>
+}
+const TopicsList = () => {
+  return <div>
+    <h1>Topic List Pages</h1>
+  </div>
+}
+
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/topics/:topicId' component={TopicDetail} />
+      <Route exact path='/topics' component={TopicsList} />
+    </Switch>
   );
 }
 
