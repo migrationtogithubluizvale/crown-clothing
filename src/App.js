545a3d980ccb1dacbcc4ef5bc57301fe9b5cf47a
Luiz Felipe Silva Vale <luiz.vale1995@gmail.com>
2020-02-28 19:49:32 -0300

    add shop page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import './App.css'
 
 import HomePage from './pages/homepage/homepage.component'
-
-const TopicDetail = (props) => {
-  console.log(props)
-  return <div>
-    <h1>Topic Details</h1>
-  </div>
-}
-const TopicsList = () => {
-  return <div>
-    <h1>Topic List Pages</h1>
-  </div>
-}
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
   return (
     <Switch>
       <Route exact path='/' component={HomePage} />
-      <Route exact path='/topics/:topicId' component={TopicDetail} />
-      <Route exact path='/topics' component={TopicsList} />
+      <Route exact path='/shop' component={ShopPage} />
     </Switch>
   );
 }
