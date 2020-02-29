3ff644d4b65864edf59ce641aec3c501c8111364
Luiz Felipe Silva Vale <luiz.vale1995@gmail.com>
2020-02-28 21:32:54 -0300

    add sign page and sign in button

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css'
 
-import HomePage from './pages/homepage/homepage.component'
-import ShopPage from './pages/shop/shop.component'
+
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignOutPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header-component/header.component';
 
 function App() {
@@ -11,6 +13,7 @@ function App() {
     <div>
       <Header />
     <Switch>
+      <Route exact path='/signin' component={SignInAndSignOutPage} />
       <Route exact path='/' component={HomePage} />
       <Route exact path='/shop' component={ShopPage} />
     </Switch>
