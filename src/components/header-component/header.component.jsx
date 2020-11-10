0e3682192b3b9ae7ed0ee2dd19ea3529b38a06c1
luizvale <luiz.vale1995@gmail.com>
2020-11-10 11:15:22 -0500

    feat: add hidden functionality for CartDropdown

--- src/components/header-component/header.component.jsx
+++ src/components/header-component/header.component.jsx
@@ -8,7 +8,7 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
 import './header-styles.scss';
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
      return (
      <div class='header'>
             <Link className='logo-container' to='/' > 
@@ -29,11 +29,12 @@ const Header = ({ currentUser }) => {
             }
             <CartIcont />
         </div>
-        <CartDropdown />
+        {hidden ? null : <CartDropdown />}
     </div>
      )
 }
-const mapStateToProps = (state) => ({
-    currentUser: state.user.currentUser
-})
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
+  });
 export default connect(mapStateToProps)(Header);
